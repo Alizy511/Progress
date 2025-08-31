52daa4561957658f6fe7d606015d47030cc8cc0c
ZubairMinhas786 <mzhassan444@gmail.com>
2025-08-30 23:46:29 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Scroll Progress Bar Logic
+window.onscroll = function () {
+  updateProgressBar();
+};
+
+function updateProgressBar() {
+  const winScroll = document.documentElement.scrollTop || document.body.scrollTop;
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrolled = (winScroll / height) * 100;
+  document.getElementById("progressBar").style.width = scrolled + "%";
+}
